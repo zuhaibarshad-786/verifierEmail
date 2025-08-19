f2df16abb30da20cb40ac99eb9a0a10c7a2a5252
zuhaibarshad <120156996+zuhaibarshad-786@users.noreply.github.com>
2025-08-19 20:28:39 +0500

    Update home.jsx

--- src/Pages/Home/home.jsx
+++ src/Pages/Home/home.jsx
@@ -77,9 +77,9 @@ export default function Home() {
           </Link>
         </main>
       </div>
-      {/* Moon image */}
-      <div className="flex justify-center bg-[#0F0E1A]">
-        <img width={900} height={99} src="/moonHorizon.png" alt="" />
+      {/* Moon horizon  */}
+      <div className="flex -mt-10 justify-center bg-[#0F0E1A]">
+        <img width={900} height={100} src="/moonHorizon.png" alt="" />
       </div>
       <EmailToolsSection />
       {/* Ready to transform section */}
